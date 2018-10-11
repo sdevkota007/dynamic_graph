e7431f15f346c8efa2d6226ba558daa060f314da
sudarshan <sdevkota007@gmail.com>
2018-10-11 00:12:52 -0400

    first commit

---
+++ simulate.py
@@ -0,0 +1,11 @@
+import numpy as np
+result = np.random.choice(['a', 'b', 'c', 'd'],
+                 10,
+                 p=[0.5, 0.1, 0.1, 0.3]
+                 )
+
+def birth():
+    pass
+
+def death():
+    pass
