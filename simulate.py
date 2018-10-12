ed268e527f5d38bb54ed81789d75d550ec1319b9
sudarshan <sdevkota007@gmail.com>
2018-10-12 00:02:20 -0400

    sample model prepared, bugs to fix

--- simulate.py
+++ simulate.py
@@ -1,11 +1,99 @@
+from __future__ import division
 import numpy as np
-result = np.random.choice(['a', 'b', 'c', 'd'],
-                 10,
-                 p=[0.5, 0.1, 0.1, 0.3]
-                 )
 
-def birth():
-    pass
 
-def death():
-    pass
+class Graph:
+    def __init__(self, p_birth):
+        self.p = p_birth
+        self.q = 1 - self.p
+        self.nodes = {
+            "1": ['2'],
+            "2": ['1','4','3'],
+            "3": ['2','4'],
+            "4": ['2','3']
+        }
+        # self.nodes = {
+        #     "1": ['3'],
+        #     "2": ['3'],
+        #     '3': ['1','2']
+        # }
+        self.num_edges_G = 0
+        self.update_num_edges()
+        #self.run()
+
+    def run(self):
+        result = np.random.choice(['birth', 'death'], 1, p=[self.p, self.q])[0]
+        print result
+        if result == 'birth':
+            self.birth()
+        elif result == 'death':
+            self.death()
+
+    def birth(self):
+        preference_probability = []
+        for node in self.nodes.keys():
+            P_nodeU = len(self.nodes[node])/(2*self.num_edges_G)
+            preference_probability.append(P_nodeU)
+
+        print preference_probability
+        print self.nodes.keys()
+        preferred_node = np.random.choice(self.nodes.keys(), 1, p=preference_probability)[0]
+        new_node = str(int(self.nodes.keys()[-1]) + 1)
+
+        print "New node: ", new_node
+        print "preferred node: ", preferred_node
+        self.update_nodes('birth', new_node, preferred_node)
+        self.update_num_edges()
+
+
+    def death(self):
+        preference_probability = []
+        for node in self.nodes.keys():
+            P_nodeU = (len(self.nodes) - len(self.nodes[node]))/ (len(self.nodes)**2 - 2*self.num_edges_G)
+            preference_probability.append(P_nodeU)
+        print preference_probability
+        print self.nodes.keys()
+        node_to_delete = np.random.choice(self.nodes.keys(), 1, p=preference_probability)[0]
+        print "Node to delete: ",node_to_delete
+        self.update_nodes('death', node_to_delete)
+        self.update_num_edges()
+
+
+    def update_num_edges(self):
+        degree_G = 0
+        for node in self.nodes:
+            degree_G = degree_G + len(self.nodes[node])
+        self.num_edges_G = degree_G/2                         # degree = 2*edges
+
+    def update_nodes(self, mode, *args):
+        if mode == 'birth':
+            new_node = args[0]
+            preferred_node = args[1]
+            self.nodes[new_node] = [preferred_node]
+            self.nodes[preferred_node].append(new_node)
+            print self.nodes
+
+
+        elif mode == 'death':
+            node_to_delete = args[0]
+            #delete the selected node from the graph
+            self.nodes.pop(node_to_delete, None)
+            for node in self.nodes.keys():
+                #delete the edge from all the other nodes that was connected to the deleted node
+                if node_to_delete in self.nodes[node]:
+                    self.nodes[node].remove(node_to_delete)
+
+                # check if the node from which we deleted an edge has no edges left,
+                # in that case delete that node, which is hanging without an edge
+                if not self.nodes[node]:
+                    self.nodes.pop(node, None)
+
+            print self.nodes
+
+
+if __name__ == '__main__':
+    g = Graph(0.7)
+    for i in range(100):
+        print "*****************TIME STEP {}********************".format(i)
+        g.run()
+        print "\n \n"
